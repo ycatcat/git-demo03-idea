5b318429f7f76eac784c0091a86f1bad1155f010
yykawaii <1473458121@qq.com>
2021-05-19 11:55:19 +0800

    合并分支测试

--- src/com/yangyi/demo/Hello.java
+++ src/com/yangyi/demo/Hello.java
@@ -4,4 +4,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
     }
+    public void test(){
+        System.out.println("合并分支用的测试方法");
+    }
 }
